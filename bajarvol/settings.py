353789c9ada317b8aad29ccb114bb5a92208ae19
aabdurakhmanov <aabdurakhmanov98@gmail.com>
2025-07-21 18:55:24 +0200

    postgresql ulandi

--- bajarvol/settings.py
+++ bajarvol/settings.py
@@ -106,12 +106,18 @@ WSGI_APPLICATION = 'bajarvol.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'your_db_name',
+        'USER': 'your_db_user',
+        'PASSWORD': 'your_password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
