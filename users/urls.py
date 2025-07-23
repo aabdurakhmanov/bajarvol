cd74e2d541fd512fb89aa5e2bfefbf80fe331a8d
aabdurakhmanov <aabdurakhmanov98@gmail.com>
2025-07-23 20:58:32 +0200

    urls

--- users/urls.py
+++ users/urls.py
@@ -5,7 +5,7 @@ from .views import UserRegisterView, UserDetailView, confirm_email, CustomTokenO
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='user-register'),
-    path('me/', UserDetailView.as_view(), name='user-detail'),
+    path('profile/', UserDetailView.as_view(), name='user-detail'),
     path('confirm-email/<int:uid>/<str:token>/', confirm_email, name='confirm-email'),
     path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('contact/', contact_view, name='contact'),
